78a97fbf32ef082725d04087187be9c22e347bd9
Atul Kumar <mycodingx@gmail.com>
2019-02-26 21:57:31 +0530

    js array function

---
+++ script.js
@@ -0,0 +1,27 @@
+/** Properties in JS */
+ var pens = ['red','black','green','yellow'];
+ 
+ /** Length Property */
+ //var Length = pens.length;
+ //console.log(Length);
+
+ /**Function in JS */
+ /** Build in functions (Array functions) */
+
+
+/** 1. toString() */
+//var penString = pens.toString();
+//console.log(penString);
+
+/**2. push() */
+//pens.push('blue','orange');
+//console.log(pens);
+
+/**3. pop() */
+//pens.pop();
+//console.log(pens);
+
+
+
+
+
